bbefbcb48db66f8f3cea734835315b60f6a19a89
zanestokes <zane.stokes@koretechx.com>
2024-05-23 11:16:38 +0500

    feat(n5-10): create change password api

--- helper/validation_schema.js
+++ helper/validation_schema.js
@@ -1,47 +1,50 @@
 const joi = require("joi");
 
-
 const authSchema = joi.object({
+  registration_type: joi.string()?.lowercase().required(),
+  couples_type: joi.string().lowercase().required(),
+  event_fee: joi.number().required(),
+  profile_pic: joi.string().required(),
+  is_fee_paid: joi.boolean(),
+  gender: joi.string().required(),
+  date_of_birth: joi.string(),
+  phone_number: joi.string().regex(/^(?:\+?44|0)7(?:\d{3}|\d{4})\d{6}$/),
+  full_name: joi.string().required(),
+  address: joi?.string().required(),
+  occupation: joi?.string().required(),
+  height: joi?.number().required(),
+  weight: joi?.number().required(),
+  sexuality: joi.string().required(),
+  life_style: joi.string().required(),
+  wanted_experience: joi.string().required(),
+  user_quality: joi.string().required(),
+  is_agree_terms_and_conditions: joi.boolean().required(),
+  user_status: joi.string(),
+  email: joi.string().email().lowercase().required(),
+});
 
-    registration_type : joi.string()?.lowercase().required(),
-    couples_type:  joi.string().lowercase().required(),
-    event_fee : joi.number().required(),
-    profile_pic : joi.string().required(),
-    is_fee_paid: joi.boolean(),
-    gender: joi.string().required(),
-    date_of_birth : joi.string(),
-    phone_number: joi.string().regex(/^(?:\+?44|0)7(?:\d{3}|\d{4})\d{6}$/),
-    full_name: joi.string().required(),
-    address : joi?.string().required(),
-    occupation:joi?.string().required(),
-    height:joi?.number().required(),
-    weight:joi?.number().required(),
-    sexuality : joi.string().required(),
-    life_style : joi.string().required(),
-    wanted_experience : joi.string().required(),
-    user_quality : joi.string().required(),
-    is_agree_terms_and_conditions : joi.boolean().required(),
-    user_status : joi.string(),
-    email: joi.string().email().lowercase().required(),
-})
-
-
-const loginSchema = joi.object({
+const loginSchema = joi
+  .object({
     password: joi.string().required(),
     membership_id: joi.string(),
-    username: joi.string()
-}).or('membership_id', 'username').required();
-
+    username: joi.string(),
+  })
+  .or("membership_id", "username")
+  .required();
 
 const approvedUserSchema = joi.object({
-    email: joi.string().email().lowercase().required(),
-})
+  email: joi.string().email().lowercase().required(),
+});
 
-
-
+const changePasswordSchema = joi.object({
+  email: joi.string().email().lowercase().required(),
+  password: joi.string().required(),
+  confirm_password: joi.ref("password"),
+});
 
 module.exports = {
-    authSchema : authSchema,
-    loginSchema: loginSchema,
-    approvedUserSchema : approvedUserSchema
-}
+  authSchema: authSchema,
+  loginSchema: loginSchema,
+  approvedUserSchema: approvedUserSchema,
+  changePasswordSchema: changePasswordSchema,
+};
