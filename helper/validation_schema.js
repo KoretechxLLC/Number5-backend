56c4d96e70b38e4c53ae8b9728149a87101c60c1
zanestokes <zane.stokes@koretechx.com>
2024-05-23 18:02:46 +0500

    feat(n5-13): create API for editing user details

--- helper/validation_schema.js
+++ helper/validation_schema.js
@@ -20,6 +20,7 @@ const authSchema = joi.object({
   user_quality: joi.string().required(),
   is_agree_terms_and_conditions: joi.boolean().required(),
   user_status: joi.string(),
+  id: joi.string(),
   email: joi.string().email().lowercase().required(),
 });
 
