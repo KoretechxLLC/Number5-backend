614cda8949c86ae70e233d08bb13b43acaab65cb
zanestokes <zane.stokes@koretechx.com>
2024-05-23 18:02:46 +0500

    feat(n5-13): create API for editing user details

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -1,5 +1,8 @@
 const createError = require("http-errors");
 const UserModel = require("../Models/user.model");
+const { authSchema } = require("../../helper/validation_schema");
+const path = require("path");
+const fs = require("fs");
 
 const UserController = {
   get: async (req, res, next) => {
@@ -10,16 +13,14 @@ const UserController = {
         throw createError.BadRequest();
       }
 
-      let user = await UserModel.findById(id)
+      let user = await UserModel.findById(id);
 
-      console.log(user,"user")
+      console.log(user, "user");
 
-      if(!user || user?.length == 0){
-
-            throw createError.NotFound("User Not Found")
-        
+      if (!user || user?.length == 0) {
+        throw createError.NotFound("User Not Found");
       }
-     
+
       let dataToSend = {
         registration_type: user?.registration_type,
         couples_type: user?.couples_type,
@@ -40,23 +41,78 @@ const UserController = {
         user_quality: user?.user_quality,
         user_status: user?.user_status,
         is_fee_paid: user?.is_fee_paid,
-        profile_pic : user?.profile_pic,
+        profile_pic: user?.profile_pic,
         is_agree_terms_and_conditions: user?.is_agree_terms_and_conditions,
         role: user?.role,
       };
 
       res.status(200).json({
-        message : "User data successfully retrived",
-        data : dataToSend
-      })
-
-
+        message: "User data successfully retrived",
+        data: dataToSend,
+      });
     } catch (err) {
+      next(err);
+    }
+  },
+  put: async (req, res, next) => {
+    try {
+      const userData = req.body;
+      const { id } = userData;
+
+      let filename = req?.file?.filename;
+
+      if (filename) {
+        userData.profile_pic = filename;
+      }
+      if (!id) {
+        throw createError.BadRequest("Required fields are missing");
+      }
+
+      await authSchema.validateAsync(userData);
+
+      const user = await UserModel.findById(id);
+
+      if (!user) {
+        throw createError.NotFound("User Not Found");
+      }
+
+      let oldProfilePicFilename = user.profile_pic;
+
+      if (filename) {
+        const destinationFolder = path.join(
+          __dirname,
+          `../../profileImages/${oldProfilePicFilename}`
+        );
+
+        fs.unlink(destinationFolder, (err) => {
+          if (err) {
+            console.error("error deleting picture");
+          }
+        });
+      }
+
+      const updatedUser = await UserModel.findByIdAndUpdate(id, userData, {
+        new: true,
+      });
+
+      if (!updatedUser) {
+        throw createError.NotFound("User Not Found");
+      }
+
+      let dataToSend = { ...updatedUser._doc };
+      delete dataToSend.password;
+
+      res.status(200).json({
+        message: "User updated successfully",
+        data: dataToSend,
+      });
+    } catch (err) {
+        
+      if (err.isJoi) return next(createError.BadRequest());
+
       next(err);
     }
   },
 };
 
-
-
-module.exports = UserController
+module.exports = UserController;
--- src/Routes/ProtectedRoutes/ProtectedRoutes.js
+++ src/Routes/ProtectedRoutes/ProtectedRoutes.js
@@ -2,9 +2,13 @@ const express = require("express");
 const { verifyAccessToken } = require("../../../helper/jwt_helper");
 const { verifyAdminRole } = require("../../../helper/check_role");
 const UserController = require("../../Controllers/user.controller");
+const uploadProfileImage = require("../../../utils/upload.profileImage");
 
 const router = express.Router();
 
 router.get("/get-user-data/:id", verifyAccessToken, UserController.get);
+
+
+router.put("/edit-user-data/", verifyAccessToken,uploadProfileImage.single('profileImage'),UserController.put);
 
 module.exports = router;
