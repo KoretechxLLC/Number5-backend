68abb7f6be71fb70f664a5ea41b8050857a93629
zanestokes <zane.stokes@koretechx.com>
2024-05-24 12:03:45 +0500

    feat(n5-15): create terms and conditions API

---
+++ src/Controllers/terms_and_condition.controller.js
@@ -0,0 +1,40 @@
+const createError = require("http-errors");
+const { TermsAndConditionsSchema } = require("../../helper/validation_schema");
+const TermsAndConditionsModel = require("../Models/termsAndConditions.model");
+
+const TermsAndConditionsController = {
+
+  addTermsAndConditions: async (req, res, next) => {
+    try {
+      let { terms_and_conditions } = req.body;
+
+      console.log(req.body,"body")
+
+      if (!terms_and_conditions){
+        throw createError.BadRequest("Required fields are missing");
+    }
+
+      let result = await TermsAndConditionsSchema.validateAsync(
+        req.body
+      );
+
+      let termsAndConditions = new TermsAndConditionsModel({
+        terms_and_conditions: result?.terms_and_conditions,
+      });
+
+      let data = await termsAndConditions.save();
+
+      res.status(200).json({
+        message: "Terms and Conditions Successfully Updated",
+        data: data,
+      });
+    } catch (err) {
+
+      if (err.isJoi) return next(createError.BadRequest(err?.message));
+      next(err);
+    }
+  },
+};
+
+
+module.exports = TermsAndConditionsController
